9ae0a412febaadb9b3229c2f3e350bbdbfc0ebe8
Joey <joewoodnyc@gmail.com>
2021-06-22 17:47:36 -0400

    heroku ready

--- models/routes/api.js
+++ models/routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Transaction = require("../models/transaction.js");
+const Transaction = require("../transaction.js");
 
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
